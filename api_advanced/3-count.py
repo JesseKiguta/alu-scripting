4e17d32790570d4cca02759202a99cf0b2f692ca
JesseKiguta <j.kiguta@alustudent.com>
2023-11-11 18:36:11 +0000

    pep8 for 3

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """recursive function that queries the Reddit API,
 parses the title of all hot articles,
-and prints a sorted count of given keywords 
+and prints a sorted count of given keywords
 (case-insensitive, delimited by spaces)"""
 import requests
 
